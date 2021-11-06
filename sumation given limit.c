d76815ed88c6390e818f550bdfc31f4b611e337e
MOHAMMED SHAMEEL <vkshameel@gmail.com>
2021-11-06 20:28:03 +0530

    added sumation given limit

---
+++ sumation given limit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i,j,sum=0;
+    printf("enter the limit");
+    scanf("%d",&j);
+    for(i=1;i<=j;i++)
+    {
+        sum=sum+i;
+    
+    }
+    printf("%d",sum);
+
+    
+}
